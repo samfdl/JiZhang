b69fc1ee9fbea04d3331eecbe72dc788d808086d
冯德林 <samfdl@163.com>
2018-10-27 16:35:33 +0800

    添加 支出类别页面

--- app/src/main/java/com/samfdl/jizhang/PayFragment.java
+++ app/src/main/java/com/samfdl/jizhang/PayFragment.java
@@ -4,21 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.samfdl.jizhang.activity.PayCategoryActivity;
 
 /**
  * Created by sam on 2018/10/27.
  */
 public class PayFragment extends Fragment implements View.OnClickListener {
-    private Button create;
+    private View icon;
+    private TextView kind;
+    private EditText sum;
+
+    private TextView classify;
+
+    private RecyclerView normal_kind;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_pay, container, false);
+
+        icon = rootView.findViewById(R.id.icon);
+        kind = rootView.findViewById(R.id.kind);
+        kind.setOnClickListener(this);
+        sum = rootView.findViewById(R.id.sum);
+
+        classify = rootView.findViewById(R.id.classify);
+
+        normal_kind = rootView.findViewById(R.id.normal_kind);
 
         return rootView;
     }
@@ -26,7 +45,7 @@ public class PayFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         Intent intent = new Intent();
-        intent.setClass(getActivity(), CreateActivity.class);
+        intent.setClass(getActivity(), PayCategoryActivity.class);
         startActivity(intent);
     }
 }
